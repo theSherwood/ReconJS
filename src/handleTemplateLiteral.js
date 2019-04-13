555afe5c84b545e23e7ad4a46348b7ac89db6b1c
admls <adam.the.sherwood@gmail.com>
2019-04-13 13:09:40 +0100

    handles simple `...`

--- src/handleTemplateLiteral.js
+++ src/handleTemplateLiteral.js
@@ -1,16 +1,19 @@
 "use strict";
 
 (function() {
+  const u = require("./utils");
+
   const handleTemplateLiteral = (i, str) => {
     const templateLiteralStacks = [["`"]];
-    let templateSegment = 1;
+    let templateSegment = 0;
     let templateExpressionFlag = false;
     const templateExpression = [];
     for (let j = i + 1; j < str.length; j++) {
+      // debugger;
       switch (true) {
         case templateExpressionFlag:
           templateExpression.push(str[j]);
-          if (str[j] === "}" && !isEscaped(j, str)) {
+          if (str[j] === "}" && !u.isEscaped(j, str)) {
             templateLiteralStacks.push(
               ...split(templateExpression.slice(1, -1).join(""))
             );
@@ -20,14 +23,16 @@
             templateLiteralStacks.push([]);
           }
           break;
-        case str[j] === "$" && !isEscaped(j, str):
+        case str[j] === "$" && !u.isEscaped(j, str):
           if (j + 1 < str.length && str[j + 1] === "{") {
             templateExpressionFlag = true;
           }
           break;
-        case str[j] === "`" && !isEscaped(j, str):
+        case str[j] === "`" && !u.isEscaped(j, str):
           templateLiteralStacks[templateSegment].push(str[j]);
-          // data.segments.push(...templateLiteralStacks);
+          templateLiteralStacks[templateSegment] = templateLiteralStacks[
+            templateSegment
+          ].join("");
           return templateLiteralStacks;
         default:
           templateLiteralStacks[templateSegment].push(str[j]);
