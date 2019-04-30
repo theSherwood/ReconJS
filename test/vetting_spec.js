a9349cd961a9a2f520c8541f0e29ecb1a9aea618
admls <adam.the.sherwood@gmail.com>
2019-04-30 21:50:03 +0100

    unvet removes multiple entries in an array

--- test/vetting_spec.js
+++ test/vetting_spec.js
@@ -3,17 +3,27 @@
 const { vetMethods } = require("../src/vetting");
 
 describe("vetMethods", () => {
-  let vettedHash;
-
-  beforeEach(() => {
-    vettedHash = vetMethods.getDefaultVetted();
-  });
-
   describe("unvet", () => {
+    let vettedHash;
+
+    beforeEach(() => {
+      vettedHash = vetMethods.getDefaultVetted();
+    });
+
     it("allows removing a string from the vettedHash object", () => {
       expect(vettedHash["do"]).toBe(1);
       vetMethods.unvet("do", vettedHash);
       expect(vettedHash["do"]).toEqual(undefined);
+    });
+
+    it("allows removing multiple entries with an array of strings", () => {
+      expect(vettedHash["do"]).toBe(1);
+      expect(vettedHash["let"]).toBe(1);
+      expect(vettedHash["const"]).toBe(1);
+      vetMethods.unvet(["do", "let", "const"], vettedHash);
+      expect(vettedHash["do"]).toEqual(undefined);
+      expect(vettedHash["let"]).toEqual(undefined);
+      expect(vettedHash["const"]).toEqual(undefined);
     });
   });
 });
