ae9c52332b00de4a931dc70c0ff63f027d9d8843
admls <adam.the.sherwood@gmail.com>
2019-05-25 02:28:34 +0100

    build scope on the astArray

--- src/buildScopeTree.js
+++ src/buildScopeTree.js
@@ -1,15 +1,34 @@
-function buildScopeTree(ast, walker) {
-  // ast.fscope = ast;
-  // ast.bscope = ast;
+// function buildScopeTree(ast, walker) {
+//   // ast.fscope = ast;
+//   // ast.bscope = ast;
 
-  walker(ast, undefined, childCB);
-  return ast;
-}
+//   walker(ast, undefined, childCB);
+//   return ast;
+// }
 
-function childCB(value, node) {
-  value.fscope = functionScopeNodes.includes(node.type) ? node : node.fscope;
+// function childCB(value, node) {
+//   value.fscope = functionScopeNodes.includes(node.type) ? node : node.fscope;
 
-  value.bscope = blockScopeNodes.includes(node.type) ? node : node.bscope;
+//   value.bscope = blockScopeNodes.includes(node.type) ? node : node.bscope;
+// }
+
+function buildScopeTree(astArray, walker) {
+  walker(astArray, node => {
+    const parent = astArray[node.parent];
+    if (parent) {
+      if (parent.functionScope === undefined) console.log("fscope ", parent);
+      node.functionScope = functionScopeNodes.includes(parent.type)
+        ? parent.index
+        : parent.functionScope;
+      if (parent.blockScope === undefined) console.log("bscope ", parent);
+      node.blockScope = blockScopeNodes.includes(parent.type)
+        ? parent.index
+        : parent.blockScope;
+    } else {
+      node.functionScope = node.index;
+      node.blockScope = node.index;
+    }
+  });
 }
 
 const functionScopeNodes = [
