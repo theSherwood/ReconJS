04149d5612412473fac5b7d0d66ef698cfcd39c6
admls <adam.the.sherwood@gmail.com>
2019-05-26 15:18:14 +0100

    add paramsHandler

--- src/buildScopeTree.js
+++ src/buildScopeTree.js
@@ -1,42 +1,61 @@
-// function buildScopeTree(ast, walker) {
-//   // ast.fscope = ast;
-//   // ast.bscope = ast;
+/*
 
-//   walker(ast, undefined, childCB);
-//   return ast;
-// }
-
-// function childCB(value, node) {
-//   value.fscope = functionScopeNodes.includes(node.type) ? node : node.fscope;
-
-//   value.bscope = blockScopeNodes.includes(node.type) ? node : node.bscope;
-// }
-
+*/
 function buildScopeTree(astArray, walker) {
   walker(astArray, node => {
     scopeHandler(astArray, node);
-
-    // TODO: params handler ?
-    // The params handler and the variables handler
-    // might need to be done recursively on the tree
-    // rather than iteratively on the array.
+    paramsHandler(node);
   });
 }
 
+/* 
+    The astArray is a tree structure flattened into an array.
+    For any node x, the nodes in the subtree of x must all
+    have higher indices than the index of x.
+
+    For any node x, functionScope and blockScope property 
+    values are the index of the ancestor node in the array 
+    that creates that scope for the node in question.
+  */
 function scopeHandler(astArray, node) {
   const parent = astArray[node.parent];
   if (parent) {
-    if (parent.functionScope === undefined) console.log("fscope ", parent);
     node.functionScope = functionScopeNodes.includes(parent.type)
       ? parent.index
       : parent.functionScope;
-    if (parent.blockScope === undefined) console.log("bscope ", parent);
-    node.blockScope = blockScopeNodes.includes(parent.type)
-      ? parent.index
-      : parent.blockScope;
+
+    // blockScope must be at least as specific as functionScope
+    node.blockScope = Math.max(
+      blockScopeNodes.includes(parent.type) ? parent.index : parent.blockScope,
+      node.functionScope
+    );
   } else {
     node.functionScope = node.index;
     node.blockScope = node.index;
+  }
+}
+
+/*
+    The ast already contains a params object on function
+    nodes. However, not all identifiers within that object
+    need to be cleared for use within the function. For
+    example: the left side identifier of an AssignmentPattern is free to use inside the function. Such params will be added to an array called scopedParams. The right side, however, may pose a security threat.
+  */
+function paramsHandler(node) {
+  if (node.hasOwnProperty("params")) {
+    const params = node.params;
+    const scopedParams = [];
+    for (let i = 0; i < params.length; i++) {
+      const param = params[i];
+      if (param.type === "Identifier") {
+        scopedParams.push(param.name);
+      } else if (param.type === "AssignmentPattern") {
+        if (param.left.type === "Identifier") {
+          scopedParams.push(param.left.name);
+        }
+      }
+    }
+    node.scopedParams = scopedParams;
   }
 }
 
