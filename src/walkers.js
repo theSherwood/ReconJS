6c27ca0ad72297262da78e8255f5310e04538729
admls <adam.the.sherwood@gmail.com>
2019-05-27 11:45:18 +0100

    abort recursion if the prop isn't a proper node

--- src/walkers.js
+++ src/walkers.js
@@ -6,21 +6,31 @@ export function traverseASTArray(astArray, callback) {
 
 export function recurseAST(node, nodeCallback, childCallback, state) {
   (function recurse(node, nodeCallback, childCallback, state) {
-    // Do not use Booleans or anything that evaluates to false as state
+    /*
+      Do not use Booleans or anything that evaluates to false as state
+      If you need to have access to falsy values, wrap your state
+      in an object.
+    */
     let newState = nodeCallback ? nodeCallback(node, state) : state;
     Object.values(node).forEach(value => {
       // Iterate over properties on the node, looking for
       // child nodes.
       if (typeof value === "object" && value !== null) {
-        // if (value.hasOwnProperty("index")) console.log(value);
         if (Array.isArray(value)) {
+          // value isn't a node. It is an array of nodes.
+          // Skip the array and start on the children.
           value.forEach(arrayChild => {
             newState = childCallback
               ? childCallback(arrayChild, node, newState)
               : newState;
             recurse(arrayChild, nodeCallback, childCallback, newState);
           });
+        } else if (!value.hasOwnProperty("type")) {
+          // If value isn't an object with 'type' property, it
+          // isn't a proper node. Abort.
+          return;
         } else {
+          // value is a proper AST node.
           newState = childCallback
             ? childCallback(value, node, newState)
             : newState;
